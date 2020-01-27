c1cbd8cff28272fffd40c74ee800bf84c4efc238
smv5047 <smv5047@gmail.com>
2020-01-26 20:04:12 -0500

    completed the auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,18 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
+const secret = require("../config/secret");
+
 
 module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, secret.jwt)
+
+    req.users = decoded.id
+    next()
+  } catch (err) {
   res.status(401).json({ you: 'shall not pass!' });
+  }
 };
